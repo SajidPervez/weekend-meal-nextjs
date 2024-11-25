150386a23cfd21d92108a077dd0e1f0002a9caae
SajidPervez <sajidpervez@gmail.com>
2024-11-25 19:00:25 +1100

    stipe key added

--- app/api/create-checkout-session/route.ts
+++ app/api/create-checkout-session/route.ts
@@ -8,7 +8,7 @@ interface CheckoutBody {
   customerPhone: string;
 }
 
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {  
   apiVersion: '2024-11-20.acacia',
 });
 
