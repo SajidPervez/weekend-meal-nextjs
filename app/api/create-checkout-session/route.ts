0d320a889fa5d737dc63b22c6f5f7d8cee7e62f7
SP <sajid.233@gmail.com>
2024-11-29 17:27:54 +1100

    changes to route

--- app/api/create-checkout-session/route.ts
+++ app/api/create-checkout-session/route.ts
@@ -20,7 +20,7 @@ interface CheckoutBody {
 }
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
-  apiVersion: '2024-11-20.acacia',
+  apiVersion: '2024-11-20.acacia', 
   typescript: true,
 });
 
